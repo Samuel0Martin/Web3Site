475f3acfd8431610ec6620df8a2cdb1de6c8a7e9
Samuel Martin (1000045841) <MARTSG2@student.op.ac.nz>
2020-03-18 09:03:17 +1300

    Adding updated files

--- app.py
+++ app.py
@@ -1,42 +1,81 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from random import random
 from math import floor
+from mongoengine import *
 
 app = Flask(__name__)
 
+connect('web3_DB')
+
+class User(Document):
+    email = StringField()
+    first_name = StringField()
+    last_name = StringField()
+
+class Country(Document):
+    name = StringField()
 
 
 @app.route('/')
-@app.route('/index')
-@app.route('/home')
-def index():
-    return render_template('index.html')
+def hello_world():
+	bob = User(email='a123@b123', first_name='Bob', last_name='Bob2').save()
+	return 'Hello, World!'
 
-
-@app.route('/inspire')
-@app.route('/inspiration')
-def inspire():
-    return render_template('inspiration.html')
-
-
+@app.route('/createCountry')
+def create_world():
+	Country(name='Bobs Country').save()
+	return 'Created a new Country'
 
 @app.route('/page2')
 def return_world():
-    return 'This is the second page.'
+	return 'This is the second page.'
 
 @app.route('/page3')
 def great_world():
-	num = random() * 76536738953465934698348673489767583495763486374087745737374583243785273698704360473867439867483768473986723076246720750917095710752379864370984730687067026797208601
+	num = random() * 100
 	return 'This is page ' + str(floor(num)) + '.'
 
 @app.route('/page4')
 def fourth_world():
-	num = random() * 45345645348564853533321323545656989907086421414343567680709653453463546788969756434547657097954421324467609097966423
-	return 'This is the ' + str(floor(num)) + ' world of ' + str(floor(num)) + ' universe. ' + str(floor(num)) +  str(floor(num)) +  str(floor(num)) +  str(floor(num)) +  str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + ' ' + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + ' ' + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + ' ' + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + ' ' + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num)) + str(floor(num))
+	num = random() * 45345645348564853533321323546423097
+	return 'This is the page number: ' + str(floor(num))
 
+@app.route('/page5')
+def fifth_world():
+	return User.objects.to_json()
 
+@app.route('/pageCountry')
+def all_world():
+	for country in Country.objects:
+		print(country.name)
+	return Country.objects.to_json()
+	
+	
+@app.route('/countries', methods=['POST'])
+def addCountry():
+	Country(name='Country A').save()
+	return 'Created a new Country'
+	
+@app.route('/countries', methods=['GET'])
+def findCountries():
+	countries = Country.objects
+	return countries.to_json()
+	
+@app.route('/countries/<country_id>', methods=['GET'])
+def getCountry(country_id=None):
+	countries = None
+	if country_id is None:
+		countries = Country.objects
+	else:
+		countries = Country.objects.get(id=country_id)
+	return countries.to_json()
+	
+@app.route('/countries', methods=['DELETE'])
+def deleteCountry():
+	Country(name='Country A').delete()
+	return 'Removed a Country
 
 
 
 if __name__ =="__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='0.0.0.0', port=80)
