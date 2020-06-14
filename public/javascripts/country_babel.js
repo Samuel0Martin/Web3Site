cef5a98af3f420f4a012784a4b552cd4af5982a9
Samuel Martin (1000045841) <MARTSG2@student.op.ac.nz>
2020-06-15 09:02:26 +1200

    LVI

--- public/javascripts/country_babel.js
+++ public/javascripts/country_babel.js
@@ -1,24 +1,24 @@
 class CountrySearch extends Component {
-    constructor() {
-        super();
-        this.state = {
-            planets: [],
-        };
-    }
-	
+	constructor() {
+		super();
+		this.state = {
+			planets: [],
+		};
+	}
+
 	let initialPlanets = [];
-    fetch('http://10.25.138.109/countries')
-        .then(response => {
-            return response.json();
-        }).then(data => {
-        initialPlanets = data.results.map((planet) => {
-            return planet
-        });
-        console.log(initialPlanets);
-        this.setState({
-            planets: initialPlanets,
-        });
-    });	
+	fetch('http://10.25.138.109/countries')
+		.then(response => {
+			return response.json();
+		}).then(data => {
+		initialPlanets = data.results.map((planet) => {
+			return planet
+		});
+		console.log(initialPlanets);
+		this.setState({
+			planets: initialPlanets,
+		});
+	});	
 }
-	
+
 ReactDOM.render(<CountrySearch />, document.getElementById('react-search'));
