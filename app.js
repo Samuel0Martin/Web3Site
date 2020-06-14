94d5b9f3ccb7c2209e9b4d6be952a959070e819f
Samuel Martin (1000045841) <MARTSG2@student.op.ac.nz>
2020-06-15 11:25:55 +1200

    LXXXI

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var countryListRouter = require('./routes/countrylist');
 var crossGameRouter = require('./routes/crossgame');
+var endListRouter = require('./routes/endlist');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/country-list', countryListRouter);
 app.use('/cross-game', crossGameRouter);
+app.use('/end', endListRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
