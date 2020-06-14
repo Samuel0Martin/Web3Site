596565ad58268401953534405262ab34743edcf1
Samuel Martin (1000045841) <MARTSG2@student.op.ac.nz>
2020-06-15 08:51:40 +1200

    LIV

--- public/javascripts/country_babel.js
+++ public/javascripts/country_babel.js
@@ -1,15 +1,26 @@
-let initialPlanets = [];
-fetch('http://10.25.138.109/countries')
-    .then(response => {
-        return response.json();
-    }).then(data => {
-    initialPlanets = data.results.map((country) => {
-        return country
+class CountrySearch extends Component {
+    constructor() {
+        super();
+        this.state = {
+            planets: [],
+        };
+    }
+}
+
+componentDidMount() {
+    let initialPlanets = [];
+    fetch('https://swapi.co/api/planets/')
+        .then(response => {
+            return response.json();
+        }).then(data => {
+        initialPlanets = data.results.map((planet) => {
+            return planet
+        });
+        console.log(initialPlanets);
+        this.setState({
+            planets: initialPlanets,
+        });
     });
-    console.log(initialPlanets);
-    this.setState({
-		planets: initialPlanets,
-    });
-});
+}
 	
 ReactDOM.render(<CountrySearch />, document.getElementById('react-search'));
