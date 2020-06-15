77366ce0da5f2bbad4e70a5e9f2f8dd0536ac303
Samuel Martin (1000045841) <MARTSG2@student.op.ac.nz>
2020-06-15 14:03:49 +1200

    XCX

--- public/javascripts/country_babel.js
+++ public/javascripts/country_babel.js
@@ -72,7 +72,7 @@ class Countries extends React.Component {
 					Update Country
 				</button>
 				<br/><br/>
-				<button className="del" onClick={() => this.deleteCountry({state.updateInput}) }>
+				<button className="del" onClick={() => this.deleteCountry({updateInput}) }>
 					Delete Country
 				</button>
 			</div>
