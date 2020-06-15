c881a74755520167cf59f8a964363d1c052d8bf6
Samuel Martin (1000045841) <MARTSG2@student.op.ac.nz>
2020-06-15 15:25:10 +1200

    CXXVIII

--- public/javascripts/country_babel.js
+++ public/javascripts/country_babel.js
@@ -76,7 +76,7 @@ class Countries extends React.Component {
 					Update Country
 				</button>
 				<br/><br/>
-				<button className="del" onClick{() => {this.deleteCountry({this.state.title})}}>
+				<button className="del" onClick={() => this.deleteCountry({this.state.title}) }>
 					Delete Country
 				</button>
 			</div>
